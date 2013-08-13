c84fe6465d93a57accc156af8406b198c9737d40
zengzhu <zeng.zhu@gmail.com>
2013-08-13 20:27:40 +0800

    update clould code.

--- cloud/main.js
+++ cloud/main.js
@@ -1,5 +1,16 @@
-// Use AV.Cloud.define to define as many cloud functions as you want.
-// For example:
-AV.Cloud.define("hello", function(request, response) {
-  response.success("Hello world From avos cloud!");
+AV.Cloud.define("averageStars", function(request, response) {
+  var query = new AV.Query("Review");
+  query.equalTo("movie", request.params.movie);
+  query.find({
+    success: function(results) {
+      var sum = 0;
+      for (var i = 0; i < results.length; ++i) {
+        sum += results[i].get("stars");
+      }
+      response.success(sum / results.length);
+    },
+    error: function() {
+      response.error("movie lookup failed");
+    }
+  });
 });
